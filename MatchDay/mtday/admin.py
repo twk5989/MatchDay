e4218bdd2165502f57af720af912c921bc92a84b
twk5989 <a37225989@gmail.com>
2025-02-04 16:19:24 +0900

    UI 구성

---
+++ MatchDay/mtday/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Clubs,User, SportsFacility
+
+admin.site.register(Clubs)
+admin.site.register(User)
+admin.site.register(SportsFacility)
+
+# Register your models here.
